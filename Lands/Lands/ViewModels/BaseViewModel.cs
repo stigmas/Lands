a00b51a306a4875afd04e5dcfb29e2937d7f3a56
stigmas <melocotonero00@gmail.com>
2018-08-07 23:15:43 +0200

    Part 8 - BaseViewModel

---
+++ Lands/Lands/ViewModels/BaseViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Lands.ViewModels
+{
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
+    class BaseViewModel : INotifyPropertyChanged
+    {
+        public void RaisePropertyChanged([CallerMemberName] string propertyName= "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
+
+
--- Lands/Lands/ViewModels/LoginViewModel.cs
+++ Lands/Lands/ViewModels/LoginViewModel.cs
@@ -1,16 +1,63 @@
 ﻿
 namespace Lands.ViewModels
 {
+    using GalaSoft.MvvmLight.Command;
+    using System;
+    using System.ComponentModel;
     using System.Windows.Input;
+    using Xamarin.Forms;
 
-    class LoginViewModel
+    class LoginViewModel : BaseViewModel
     {
+        #region Attributes
+        private string password;
+        private bool isRunning;
+        private bool isEnabled;
+        #endregion
+
         #region Properties
         public string Email { get; set; }
-        public string Password { get; set; }
-        public bool IsRunning { get; set; }
-        public bool IsRemembered { get; set; }
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+            set
+            {
+                this.password = value;
+                RaisePropertyChanged("Password");
+            }
+        }
 
+        public bool IsRunning
+        {
+            get
+            {
+                return isRunning;
+            }
+            set
+            {
+                this.isRunning = value;
+                RaisePropertyChanged("IsRunning");
+            }
+        }
+
+        public bool IsEnabled
+        {
+            get
+            {
+                return isEnabled;
+            }
+            set
+            {
+                this.isEnabled = value;
+                RaisePropertyChanged("IsEnabled");
+            }
+        }
+
+        public bool IsRemembered { get; set; }
+        
         #endregion
 
         #region Constructors
@@ -18,11 +65,55 @@ namespace Lands.ViewModels
         {
             this.IsRemembered = true;
             this.IsRunning = false;
+            this.IsEnabled = true;
         }
         #endregion
 
         #region Commands
-        public ICommand LoginCommand { get; set; }
+        public ICommand LoginCommand
+        {
+            get
+            {
+                return new RelayCommand(Login);
+            }
+        }
+
+
+        private async void Login()
+        {
+            if (string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error", "You must enter an email", "Accept");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error", "You must enter a password", "Accept");
+                return;
+            }
+
+            this.IsRunning = true;
+            this.IsEnabled = false;
+
+            if (this.Email != "jgallego@eurofic.com" || this.Password != "1234")
+            {
+                this.IsRunning = false;
+                this.IsEnabled = true;
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error", "Email or password incorrect", "Accept");
+                this.Password = string.Empty;
+                return;
+            }
+
+            this.IsRunning = false;
+            this.IsEnabled = true;
+            await Application.Current.MainPage.DisplayAlert(
+                    "Informacion", "Acceso permitido", "Ok");
+            return;
+        }
         #endregion
 
     }
