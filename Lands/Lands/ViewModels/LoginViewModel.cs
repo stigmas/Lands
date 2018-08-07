81b95f0d31e5d9d0ebed79f030ce0fe779f219a4
stigmas <melocotonero00@gmail.com>
2018-08-07 19:48:13 +0200

    Initial Project

---
+++ Lands/Lands/ViewModels/LoginViewModel.cs
@@ -0,0 +1,29 @@
+﻿
+namespace Lands.ViewModels
+{
+    using System.Windows.Input;
+
+    class LoginViewModel
+    {
+        #region Properties
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool IsRunning { get; set; }
+        public bool IsRemembered { get; set; }
+
+        #endregion
+
+        #region Constructors
+        public LoginViewModel()
+        {
+            this.IsRemembered = true;
+            this.IsRunning = false;
+        }
+        #endregion
+
+        #region Commands
+        public ICommand LoginCommand { get; set; }
+        #endregion
+
+    }
+}
---
+++ Lands/Lands/ViewModels/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lands.ViewModels
+{
+    class MainViewModel
+    {
+        #region ViewModels
+        public LoginViewModel Login { get; set; }
+        #endregion
+
+        #region Constructor
+        public MainViewModel()
+        {
+            this.Login = new LoginViewModel();
+        }
+        #endregion
+    }
+}
