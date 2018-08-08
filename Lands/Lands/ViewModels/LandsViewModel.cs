7e71fd06a51e39bb6ec95a5552c600251b280d93
stigmas <melocotonero00@gmail.com>
2018-08-08 14:18:14 +0200

    Video 12

--- Lands/Lands/ViewModels/LandsViewModel.cs
+++ Lands/Lands/ViewModels/LandsViewModel.cs
@@ -41,6 +41,17 @@
         #region Methods
         private async void LoadLands()
         {
+            var connection = await this.apiService.CheckConnection();
+            if(!connection.IsSuccess)
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                   "Error",
+                   connection.Message,
+                   "Accept");
+                await Application.Current.MainPage.Navigation.PopAsync();
+                return;
+            }
+
             var response = await this.apiService.GetList<Land>(
                 "http://restcountries.eu",
                 "/rest",
